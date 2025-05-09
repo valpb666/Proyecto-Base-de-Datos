fab193afcd9dedfee5370fb5a1dba868db77a4b0
valeboo <valentinapinedabarrn@gmail.com>
2025-05-09 12:39:17 -0600

    Create carga_inicial.py
    Documentación de cómo realizar la carga inicial del set de datos a
    una base de datos de tipo Postgres.

---
+++ carga_inicial.py
@@ -0,0 +1,44 @@
+/* ****************************** */
+/*          CARGA INICIAL         */
+/* ****************************** */
+
+CREATE TABLE staging(
+		id BIGSERIAL PRIMARY KEY,
+		-- Este atributo nos servira para pasar los datos posteriormente
+		sexo text,
+		fecha_nacimiento date,
+		nacionalidad text,
+		lengua_indigena text,
+		estado_civil text,
+		entidad_residencia text,
+		municipio_residencia text,
+		escolaridad text,
+		ocupacion text,
+		afiliacion_medica text,
+		fecha_defuncion1 date,
+		hora_defuncion time,
+		lugar_defuncion text,
+		entidad_defuncion text,
+		alcaldia text,
+		atencion_medica text,
+		necropsia text,
+		causa_defuncion text,
+		durante_embarazo text,
+		causado_embarazo text,
+		complicacion_embarazo text,
+		muerte_accidental_violenta text,
+		tipo_evento text,
+		en_trabajo text,
+		sitio_lesion text,
+		municipio_ocurrencia text,
+		fecha_defuncion date,
+		edad bigint
+	);
+
+/*
+--- PASOS EN CONSOLA ---
+
+	SET CLIENT_ENCODING TO 'UTF8';
+	
+	\copy staging(sexo, fecha_nacimiento, nacionalidad, lengua_indigena, estado_civil, entidad_residencia, municipio_residencia, escolaridad, ocupacion, afiliacion_medica, fecha_defuncion1, hora_defuncion, lugar_defuncion, entidad_defuncion, alcaldia, atencion_medica, necropsia, causa_defuncion, durante_embarazo, causado_embarazo, complicacion_embarazo, muerte_accidental_violenta, tipo_evento, en_trabajo, sitio_lesion, municipio_ocurrencia, fecha_defuncion, edad) FROM 'path_to_downloaded_csv' WITH (FORMAT CSV, HEADER true, DELIMITER ',', NULL 'NA');
+*/
