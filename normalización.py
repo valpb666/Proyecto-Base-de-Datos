a70c48aa391414e42501c70ec0451738bfe8fcf1
NuriaGarciaV <nuria.garciavaldecasas@itam.mx>
2025-05-09 19:12:20 -0600

    Update normalización.py

--- normalización.py
+++ normalización.py
@@ -1,10 +1,10 @@
 
 -- Entidad: entidad_municipio
-
+DROP TABLE persona;
 CREATE TABLE entidad_municipio (
 	id BIGSERIAL PRIMARY KEY,
-	entidad VARCHAR(200) NOT NULL,
-	municipio VARCHAR(100) NOT NULL,
+	entidad VARCHAR(200),
+	municipio VARCHAR(100),
 	
 	CONSTRAINT pares_unicos UNIQUE(entidad,municipio)
 );
@@ -26,17 +26,17 @@ WHERE NOT EXISTS (
 -- Entidad: Persona
 CREATE TABLE persona (
 	id BIGSERIAL PRIMARY KEY,
-	sexo VARCHAR(20) NOT NULL,
+	sexo VARCHAR(20),
 	fecha_nacimiento DATE,
 	lengua_indigena BOOLEAN,
-	estado_civil VARCHAR(50) NOT NULL,
+	estado_civil VARCHAR(50),
 	entidad_residencia VARCHAR(100),
 	municipio_residencia VARCHAR(100),
 	--Estos atributos residencia son temporales en lo que se ingresan los nuevos ids
 	residencia_id BIGINT,
-	escolaridad VARCHAR(200) NOT NULL,
-	ocupacion VARCHAR(200) NOT NULL,
-	afiliacion_medica VARCHAR(200) NOT NULL
+	escolaridad VARCHAR(200),
+	ocupacion VARCHAR(200),
+	afiliacion_medica VARCHAR(200)
 );
 
 INSERT INTO persona (id,sexo, fecha_nacimiento, lengua_indigena, estado_civil, entidad_residencia, municipio_residencia, escolaridad, ocupacion, afiliacion_medica)
@@ -47,20 +47,26 @@ SELECT setval('public.persona_id_seq', (SELECT MAX(id) FROM persona));
 
 UPDATE persona
 SET residencia_id = (
-	SELECT id 
-	FROM entidad_municipio
-	WHERE (entidad_municipio.entidad,entidad_municipio.municipio)=(entidad_residencia, municipio_residencia)
+    SELECT em.id
+    FROM entidad_municipio em
+    JOIN staging s ON s.entidad_residencia = em.entidad
+    AND s.municipio_residencia = em.municipio
+    WHERE s.id = persona.id
 );
+
+SELECT residencia_id
+FROM persona
+WHERE residencia_id IS NULL;
 
 ALTER TABLE persona DROP COLUMN entidad_residencia;
 ALTER TABLE persona DROP COLUMN municipio_residencia;
 
 ALTER TABLE persona
-ALTER COLUMN residencia_id SET NOT NULL,
-ALTER COLUMN residencia_id SET DEFAULT 1539, --> Tupla donde la entidad y el municipio no estan especificados
+ALTER COLUMN residencia_id SET DEFAULT 1539;--> Tupla donde la entidad y el municipio no estan especificados
+
+ALTER TABLE persona
 ADD CONSTRAINT fk_entidad_municipio
 FOREIGN KEY (residencia_id) REFERENCES entidad_municipio(id) ON DELETE SET DEFAULT;
-
 
 -- Entidad: Defuncion
 CREATE TABLE defuncion (
@@ -68,8 +74,8 @@ CREATE TABLE defuncion (
 	persona_id BIGINT UNIQUE NOT NULL CONSTRAINT fk_persona REFERENCES persona(id) ON DELETE CASCADE,
 	fecha_defuncion DATE NOT NULL,
 	hora_defuncion TIME,
-	lugar_defuncion VARCHAR(500) NOT NULL,
-	causa_defuncion VARCHAR(500) NOT NULL,
+	lugar_defuncion VARCHAR(500),
+	causa_defuncion VARCHAR(500),
 	alcaldia_defuncion_id VARCHAR (500), --valor provisional en lo que transferimos los datos
 	atencion_medica BOOLEAN,
 	necropsia BOOLEAN
@@ -99,9 +105,9 @@ FOREIGN KEY (alcaldia_defuncion_id) REFERENCES entidad_municipio(id) ON DELETE S
 CREATE TABLE embarazo (
 	id BIGSERIAL PRIMARY KEY,
 	persona_id BIGINT NOT NULL CONSTRAINT fk_persona REFERENCES persona(id) ON DELETE CASCADE,
-	durante_embarazo VARCHAR(500) NOT NULL,
-	causado_embarazo VARCHAR(200) NOT NULL,
-	complicacion_embarazo VARCHAR(200) NOT NULL
+	durante_embarazo VARCHAR(500),
+	causado_embarazo VARCHAR(200),
+	complicacion_embarazo VARCHAR(200)
 );
 
 INSERT INTO embarazo (persona_id,durante_embarazo, causado_embarazo, complicacion_embarazo)
@@ -113,6 +119,4 @@ WHERE durante_embarazo NOT ILIKE '%NO APLICA%'
     complicacion_embarazo ILIKE '%NO APLICA%'
     AND causado_embarazo ILIKE '%NO APLICA%'
   );
-
-DROP TABLE staging;
 ```
